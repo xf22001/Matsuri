faff4f65bd6a81f5dd7fcd0e8025fd68bb0ade6b
xiaofei <alibaba22001@163.com>
2023-01-15 13:53:05 +0800

    update

--- compile.sh
+++ compile.sh
@@ -6,13 +6,13 @@
 #   文件名称：compile.sh
 #   创 建 者：肖飞
 #   创建日期：2022年08月20日 星期六 12时56分41秒
-#   修改日期：2022年10月14日 星期五 10时48分53秒
+#   修改日期：2023年01月15日 星期日 13时51分16秒
 #   描    述：
 #
 #================================================================
 function main() {
 	./run init action go && ./run lib core
-	proxychains ./run init action library
+	proxychains ./run init action gradle
 	#./gradlew tasks --all
 	#./gradlew app:assembleFdroid --info
 	#./gradlew app:installFdroidDebug --info
