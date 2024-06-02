677a753c8d4f92863face8dcde3235735c4a9cc7
xiaofei <alibaba22001@163.com>
2024-06-02 20:58:41 +0800

    update

--- buildScript/lib/core/clone.sh
+++ buildScript/lib/core/clone.sh
@@ -3,6 +3,6 @@ COMMIT=$(cat libcore/core_commit.txt)
 cd ..
 [ -d v2ray-core ] && exit 0
 rm -rf v2ray-core
-git clone --no-checkout https://github.com/MatsuriDayo/v2ray-core.git
+git clone --no-checkout https://github.com/xf22001/MatsuriDayo-v2ray-core.git v2ray-core
 cd v2ray-core
 git checkout $COMMIT
